7048e13390a7fb78912f318076c90a48fbe4e86a
majny <majny24@gmail.com>
2025-03-28 22:39:39 +0100

    add back coming soon

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 buttons = ["chromeup_button", "chromesome_button", "mugshot_button", "agi_button"]
 audio_players = ["chromeup-audio", "chromesome-audio", "mugshot-audio", "agi-audio"]
 
-for (i = 0; i < 4; i++) {
-    let button = document.getElementById(buttons[i]);
-    let audio_player = document.getElementById(audio_players[i]);
+document.addEventListener("DOMContentLoaded", () => {
+    for (i = 0; i < 4; i++) {
+        let button = document.getElementById(buttons[i]);
+        let audio_player = document.getElementById(audio_players[i]);
 
-    audio_player.volume = 0.2
+        audio_player.volume = 0.2
 
-    button.addEventListener("mouseenter", () => {
-        console.log(audio_player)
-        audio_player.currentTime = 0; // Restart audio if already playing
-        audio_player.play();
-    });
+        button.addEventListener("mouseenter", () => {
+            console.log(audio_player)
+            audio_player.currentTime = 0; // Restart audio if already playing
+            audio_player.play();
+        });
 
-    button.addEventListener("mouseleave", () => {
-        audio_player.pause();
-        audio_player.currentTime = 0; // Reset audio
-    });
-}
+        button.addEventListener("mouseleave", () => {
+            audio_player.pause();
+            audio_player.currentTime = 0; // Reset audio
+        });
+    }
+})
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const comingSoon = document.querySelector('.coming-soon');
